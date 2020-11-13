ad21441333610f59de4531d88e2a818bbe110348
Oh Xyz <ohxyzcom@gmail.com>
2020-11-13 12:26:55 +1100

    added eslint

--- webpack.dev.js
+++ webpack.dev.js
@@ -13,14 +13,22 @@ module.exports = {
         contentBase: path.join( __dirname, 'public' ),
         compress: true,
         port: 5000,
-        open: true
+        open: true,
+        overlay: true
     },
     module: {
         rules: [ {
-            test: /\.js[x]{0,1}$/,
+            test: /\.jsx?$/,
             exclude: /node_modules/,
-            loader: 'babel-loader'
-        },{
+            loader: 'babel-loader',
+        }, {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'eslint-loader',
+            options: {
+                emitError: true,
+            }
+        }, {
             test: /\.css$/,
             exclude: /node_modules/,
             use: [ 'style-loader', 'css-loader' ]
